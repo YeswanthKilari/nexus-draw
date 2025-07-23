1c20d9d62b0f6ffb12b7979276ce03b0ac69fea6
yeswanth-7 <yeswanthkilari64@gmail.com>
2025-07-23 13:46:12 +0530

    restruct

--- apps/front-end/app/canvas/[roomid]/page.tsx
+++ apps/front-end/app/canvas/[roomid]/page.tsx
@@ -1,50 +1,26 @@
 "use client"
 
+import initdraw from "@/draw";
 import { Fullscreen } from "lucide-react";
 import { useEffect, useRef } from "react"
 
 export default function canvas() {
-
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
     useEffect(()=> {
         
         if (canvasRef.current) {
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d")
-
-            if (!ctx) {
-                return
-            }
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
-            canvas.addEventListener("mousedown",(e)=>{
-                clicked = true
-                startX = e.clientX
-                startY = e.clientY
-            })
-
-            canvas.addEventListener("mouseup", (e) => {
-                clicked = false
-                console.log(e.clientX)
-                console.log(e.clientY)
-            })
-
-            canvas.addEventListener("mousemove", (e) => {
-                if (clicked) { 
-                    const width = e.clientX - startX
-                    const height = e.clientY - startY
-                    ctx.clearRect(0,0,canvas.width,canvas.height)
-                    ctx.strokeRect(startX,startY, width,height)
-                }
-            })
+            initdraw(canvasRef.current)
         }
         return
         
-    },[canvasRef])
+    }, [canvasRef])
+    
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
 
     return <div>
-        <canvas ref={canvasRef} width={1920} height={1080} ></canvas>
+        <canvas ref={canvasRef} width={screenWidth} height={screenHeight} className="box-border"></canvas>
     </div>
 }
---
+++ apps/front-end/draw/index.ts
@@ -0,0 +1,71 @@
+
+type Shape = {
+    type: "rect";
+    x: number;
+    y: number;
+    width: number;
+    height : number
+} | {
+    type: "circle",
+    centerX: number;
+    centerY: number;
+    radius: number;
+}
+
+export default function initdraw(canvas: HTMLCanvasElement) {
+    const ctx = canvas.getContext("2d");
+    
+    const existingshapes: Shape[] = []
+
+
+  if (!ctx) {
+    return;
+  }
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  let clicked = false;
+  let startX = 0;
+  let startY = 0;
+  canvas.addEventListener("mousedown", (e) => {
+    clicked = true;
+    startX = e.clientX;
+    startY = e.clientY;
+  });
+
+  canvas.addEventListener("mouseup", (e) => {
+      clicked = false;
+      const width = e.clientX - startX;
+      const height = e.clientY - startY;
+      existingshapes.push({
+          type: "rect",
+          x: startX,
+          y: startY,
+          width,
+          height
+      })  
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (clicked) {
+      const width = e.clientX - startX;
+      const height = e.clientY - startY;
+      clearcanvas(existingshapes,canvas,ctx)
+      ctx.strokeStyle = "rgba(255,255,255)";
+
+      ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+
+    function clearcanvas(existingshapes: Shape[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "rgba(0,0,0)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        existingshapes.map((shape) => {
+            if (shape.type === "rect") {
+                ctx.strokeStyle = "rgba(255,255,255)";
+                ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+            }
+        })
+  };
+}
