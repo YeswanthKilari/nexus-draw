990c2cc0c1907bbad8f1509fdf96d53bc7c7c786
yeswanth-7 <yeswanthkilari64@gmail.com>
2025-08-21 00:44:45 +0530

    ws_link

--- apps/front-end/app/config.ts
+++ apps/front-end/app/config.ts
@@ -1,4 +1,4 @@
 
-// export const HTTP_BACKEND = "https://draw-app.up.railway.app";
-export const HTTP_BACKEND = "http://localhost:3001";
-export const WS_URL = "ws://localhost:8080";
+export const HTTP_BACKEND = "https://draw-app.up.railway.app";
+// export const HTTP_BACKEND = "http://localhost:3001";
+export const WS_URL = "https://draw-project-ws.up.railway.app";
