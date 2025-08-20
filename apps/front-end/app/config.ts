335050872a453aa865e3993cc5e73107ed694d9e
yeswanth-7 <yeswanthkilari64@gmail.com>
2025-08-20 23:55:35 +0530

    nothing

--- apps/front-end/app/config.ts
+++ apps/front-end/app/config.ts
@@ -1,3 +1,4 @@
 
-export const HTTP_BACKEND = "https://draw-project-production.up.railway.app";
+export const HTTP_BACKEND = "https://draw-app.up.railway.app";
+// export const HTTP_BACKEND = "http://localhost:3001";
 export const WS_URL = "ws://localhost:8080";
